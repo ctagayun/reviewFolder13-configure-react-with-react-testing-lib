693abea2fa63da437b3884453e997008d0567991
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-09-19 15:58:33 -0500

    Initial COmmit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log('Hello Webpack Project Setup Manually.');
+
+const handleDeleteRecord = () => {
+    console.log(`Item being deleted =  Button was clicked`);
+   
+  };
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const ESLintPlugin = require('eslint-webpack-plugin');
+module.exports = {
+  // 1
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  // 2
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  // 3
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+  },
+  //4
+  plugins: [new ESLintPlugin()],
+  
+};
+
+/* 
+  The Webpack configuration file gives the following instructions:
+        (1) Use the src/index.js file as entry point to bundle it. If the src/index.js file imports other JavaScript files, bundle them as well.
+        (2) The bundled source code files shall result in a bundle.js file in the /dist folder.
+        (3) The /dist folder will be used to serve our application to the browser.
+        (4) Now, all source code that goes through Weback will be checked by ESLint automatically. 
+           Once you start your application, it will output an error though: "No ESLint configuration found". You need this 
+           file to define your ESLint configuration. Create it in your project's root directory 
+           on the command line: 
+           touch .eslintrc
+
+           npm install --save-dev @babel/eslint-parser
+              
+  */  
